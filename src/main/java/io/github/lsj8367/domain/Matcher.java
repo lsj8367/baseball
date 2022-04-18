1357f5897ef15ba5fc3d3150879d294a76547961
lsj8367 <lsj8367@naver.com>
2022-04-18 23:11:52 +0900

    :sparkles: 필요없는 out값 제거

--- src/main/java/io/github/lsj8367/domain/Matcher.java
+++ src/main/java/io/github/lsj8367/domain/Matcher.java
@@ -2,25 +2,17 @@ package io.github.lsj8367.domain;
 
 public class Matcher {
 
-    private static final int NONE_VALUE = 0;
-
     public Result checkNumber(final BaseBall generateNumber, final BaseBall inputNumber) {
         //여기서 볼, 스트라이크 정보 리턴
         int strikeCount = 0;
         int ballCount = 0;
-        if (!generateNumber.number().contains(String.valueOf(inputNumber.number().charAt(0)))
-            && !generateNumber.number().contains(String.valueOf(inputNumber.number().charAt(1)))
-            && !generateNumber.number().contains(String.valueOf(inputNumber.number().charAt(2)))) {
-            //아웃인경우
-            return new Result(NONE_VALUE, NONE_VALUE, 3);
-        }
 
         for (int i = 0; i < generateNumber.number().length(); i++) {
             strikeCount += isStrikeThenPlus1(i, generateNumber, inputNumber);
             ballCount += isBallThenPlus1(i, generateNumber, inputNumber);
         }
 
-        return new Result(strikeCount, ballCount, NONE_VALUE);
+        return new Result(strikeCount, ballCount);
     }
 
 
