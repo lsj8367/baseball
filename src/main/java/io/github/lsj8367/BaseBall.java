5f6e44630c79af195e4d445604af63474c8a54ee
lsj8367 <lsj8367@naver.com>
2022-04-18 20:53:44 +0900

    :fire: baseball 생성 구현

---
+++ src/main/java/io/github/lsj8367/BaseBall.java
@@ -0,0 +1,8 @@
+package io.github.lsj8367;
+
+public record BaseBall(String number) {
+    public static BaseBall create(final NumberGenerateStrategy strategy) {
+        return new BaseBall(strategy.generate());
+    }
+
+}
