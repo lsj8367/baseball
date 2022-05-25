9cc11f77e850efaf83a386b9d41f133f228d6cd1
lsj8367 <lsj8367@naver.com>
2022-05-26 00:49:53 +0900

    :sparkles: Initial Commit

---
+++ clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/exception/DifferentCurrenciesException.java
@@ -0,0 +1,10 @@
+package io.github.lsj8367.b.simple.design.business.exception;
+
+public class DifferentCurrenciesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
