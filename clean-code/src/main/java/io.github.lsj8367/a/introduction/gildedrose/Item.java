43e114579049348abe2816da2595bfe64a01fcea
lsj8367 <lsj8367@naver.com>
2022-05-29 17:10:10 +0900

    :recycle: 코드 줄맞춤

--- clean-code/src/main/java/io.github.lsj8367/a/introduction/gildedrose/Item.java
+++ clean-code/src/main/java/io.github.lsj8367/a/introduction/gildedrose/Item.java
@@ -2,22 +2,21 @@ package io.github.lsj8367.a.introduction.gildedrose;
 
 public class Item {
 
-	public String name;
+    public String name;
 
-	public int sellIn;
+    public int sellIn;
 
-	public int quality;
+    public int quality;
 
-	public Item(String name, int sellIn, int quality) {
-		this.name = name;
-		this.sellIn = sellIn;
-		this.quality = quality;
-	}
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 
-	@Override
-	public String toString() {
-		return "\n name=" + name + ", " + sellIn + ", " + quality + "\n" ;
-	}
-	
-	
+    @Override
+    public String toString() {
+        return "\n name=" + name + ", " + sellIn + ", " + quality + "\n";
+    }
+
 }
