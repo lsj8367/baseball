fe8554d5b9af4094c07e6f64e5e8e7aa95dc7810
lsj8367 <lsj8367@naver.com>
2022-06-11 13:57:29 +0900

    :recycle: 중복 제거

--- clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/student/StudentHelper.java
+++ clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/student/StudentHelper.java
@@ -40,30 +40,37 @@ public class StudentHelper {
         return "C";
     }
 
-    /**  PROBLEM 3
-     * You and your Friend are planning to enter a Subject Quiz.
-     * However, there is a marks requirement that you should attain to qualify.
-     *
-     * Return value can be YES, NO or MAYBE.
-     *
-     * YES If either of you are very good at the subject(has 80 or more marks)
-     * However, there is an exception that if either of you is not good in the subject(20 or less marks), it is NO.
-     * In all other conditions, return MAYBE.
-     *
-     * However, the definition for good and not good are 5 marks higher if the subject is Mathematics.
-     *
-     * marks1 - your marks
-     * marks2 - your friends marks
+    /*  PROBLEM 3
+     * 문제 3 귀하와 귀하의 친구는 주제 퀴즈에 참가할 계획입니다.
+     * 그러나 자격을 얻으려면 획득해야 하는 점수 요건이 있습니다.
+     * 반환 값은 YES, NO 또는 MAYBE일 수 있습니다.
+     * 둘 중 하나가 해당 과목에 매우 능숙하면(80점 이상) YES입니다.
+     * 다만, 둘 중 어느 하나라도 해당 과목에서 좋지 않은 경우(20점 이하)인 경우는 아니오(NO)로 한다.
+     * 다른 모든 조건에서는 MAYBE를 반환합니다.
+     * 다만, 교과목이 수학일 경우 좋음과 좋지 않음의 정의가 5점 더 높다.
+     * mark1 - 귀하의 점수 mark2 - 친구 표시
      */
 
     public String willQualifyForQuiz(int marks1, int marks2, boolean isMaths) {
-        if ((isMaths ? marks1 <= 25 : marks1 <= 20) || (isMaths ? marks2 <= 25 : marks2 <= 20)) {
+
+        if (isNotGood(marks1, isMaths) || isNotGood(marks2, isMaths)) {
             return "NO";
         }
-        if ((isMaths ? marks1 >= 85 : marks1 >= 80) || (isMaths ? marks2 >= 85 : marks2 >= 80)) {
+
+        if (isGood(marks1, isMaths) || (isGood(marks2, isMaths))) {
             return "YES";
         }
         return "MAYBE";
+    }
+
+    private boolean isGood(final int marks, final boolean isMaths) {
+        int extraLimit = isMaths ? 5 : 0;
+        return marks >= 80 + extraLimit;
+    }
+
+    private boolean isNotGood(final int marks, final boolean isMaths) {
+        int extraLimit = isMaths ? 5 : 0;
+        return marks <= 20 + extraLimit;
     }
 
 }
