055bb543e55dd556449345493e52eb1359f37b85
lsj8367 <lsj8367@naver.com>
2022-06-14 23:19:11 +0900

    :sparkles: 끝의 2글자만 swap하는 로직 구현

--- clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/text/TextHelper.java
+++ clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/text/TextHelper.java
@@ -3,7 +3,16 @@ package io.github.lsj8367.b.simple.design.business.text;
 public class TextHelper {
 
     public String swapLastTwoCharacters(String str) {
-        return null;
+        if (str.length() < 2) {
+            return str;
+        }
+
+        final char back = str.charAt(str.length() - 1);
+        final char front = str.charAt(str.length() - 2);
+
+        final String front2Chars = str.substring(0, str.length() - 2);
+
+        return front2Chars + back + front;
     }
 
     public String truncateAInFirst2Positions(String str) {
