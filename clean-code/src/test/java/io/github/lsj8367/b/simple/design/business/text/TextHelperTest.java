9d4cbf65306c402f55b15be52552f69fa4645402
lsj8367 <lsj8367@naver.com>
2022-06-14 23:56:15 +0900

    :white_check_mark: 문자열 앞 2글자 A제거 기능 테스트

--- clean-code/src/test/java/io/github/lsj8367/b/simple/design/business/text/TextHelperTest.java
+++ clean-code/src/test/java/io/github/lsj8367/b/simple/design/business/text/TextHelperTest.java
@@ -2,14 +2,16 @@ package io.github.lsj8367.b.simple.design.business.text;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Disabled;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class TextHelperTest {
 
-    private TextHelper helper = new TextHelper();
+    private final TextHelper helper = new TextHelper();
 
     @Test
     @DisplayName("문자열을 null 넣을 시 에러 발생")
@@ -19,10 +21,16 @@ class TextHelperTest {
             .hasMessage("문자열은 비어있을 수 없습니다.");
     }
 
-    @Test
-    @DisplayName("빈문자열이면 자기자신을 반환")
-    void emptyStringReturnSelf() {
-        assertThat(helper.swapLastTwoCharacters("")).isEqualTo("");
+    private static Stream<Arguments> truncateAInFirst2PositionsTest() {
+        final TextHelper helper = new TextHelper();
+        return Stream.of(
+            Arguments.of(helper.truncateAInFirst2Positions("ABCD"), "BCD"),
+            Arguments.of(helper.truncateAInFirst2Positions("AACD"), "CD"),
+            Arguments.of(helper.truncateAInFirst2Positions("BACD"), "BCD"),
+            Arguments.of(helper.truncateAInFirst2Positions("BBAA"), "BBAA"),
+            Arguments.of(helper.truncateAInFirst2Positions("bbaa"), "BBAA"),
+            Arguments.of(helper.truncateAInFirst2Positions("abab"), "BAB")
+        );
     }
 
     @Test
@@ -44,13 +52,36 @@ class TextHelperTest {
     }
 
     @Test
-    @Disabled
-    public void testTruncateAInFirst2Positions() {
-        assertEquals("", helper.truncateAInFirst2Positions(""));
-        assertEquals("BCD", helper.truncateAInFirst2Positions("ABCD"));
-        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-        assertEquals("BCD", helper.truncateAInFirst2Positions("BACD"));
-        assertEquals("BBAA", helper.truncateAInFirst2Positions("BBAA"));
+    @DisplayName("빈문자열이면 자기자신을 반환")
+    void emptyStringReturnSelf() {
+        assertThat(helper.swapLastTwoCharacters("")).isEmpty();
+    }
+
+    @Test
+    @DisplayName("null 문자열이 오면 예외 테스트")
+    void nullStringExceptionTest() {
+        assertThatThrownBy(() -> helper.truncateAInFirst2Positions(null))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("문자열은 비어있을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("빈문자열을 그대로 반환")
+    void emptyStringReturnTest() {
+        assertThat(helper.truncateAInFirst2Positions("")).isEmpty();
+    }
+
+    @Test
+    @DisplayName("문자열 길이 2 미만인 경우 A를 빈문자열로 변환")
+    void ifLessThan2ThenReplaceBlank() {
+        assertThat(helper.truncateAInFirst2Positions("A")).isEmpty();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("케이스 전부 테스트")
+    void truncateAInFirst2PositionsTest(final String actual, final String expected) {
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
