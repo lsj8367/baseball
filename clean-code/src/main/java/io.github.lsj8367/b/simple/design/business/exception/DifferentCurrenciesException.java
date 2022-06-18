88a730d20a9178ed3de17817417494301c39acd2
lsj8367 <lsj8367@naver.com>
2022-06-18 14:57:45 +0900

    :recycle: Exception -> 상태예외로 변경

--- clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/exception/DifferentCurrenciesException.java
+++ clean-code/src/main/java/io.github.lsj8367/b/simple/design/business/exception/DifferentCurrenciesException.java
@@ -1,6 +1,6 @@
 package io.github.lsj8367.b.simple.design.business.exception;
 
-public class DifferentCurrenciesException extends Exception {
+public class DifferentCurrenciesException extends IllegalStateException {
 
 	private static final long serialVersionUID = 1L;
 
