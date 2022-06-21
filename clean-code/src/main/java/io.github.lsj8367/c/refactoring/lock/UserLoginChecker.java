a137d7ede9eb58d0a2860b7e3f9ed310ac2b4f3f
lsj8367 <lsj8367@naver.com>
2022-06-22 00:12:52 +0900

    :recycle: 공통부분 통합 리팩토링, if-else early return

--- clean-code/src/main/java/io.github.lsj8367/c/refactoring/lock/UserLoginChecker.java
+++ clean-code/src/main/java/io.github.lsj8367/c/refactoring/lock/UserLoginChecker.java
@@ -2,6 +2,7 @@ package io.github.lsj8367.c.refactoring.lock;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class UserLoginChecker {
 
@@ -9,46 +10,53 @@ public class UserLoginChecker {
      * {@inheritDoc}.
      */
     public Lock isUserAllowedToLogin(long id, String status,
-        boolean firstScreen, User user, List list) {
+        boolean isFirstScreen, User user, List<Object> list) {
+        if (list.isEmpty()) {
+            Lock lock = new Lock();
+            lock.setRead(false);
+            return lock;
+        }
+
+        Object[] object = (Object[]) list.get(0);
+        String lockUserId = (String) object[0];
+        Date lockTimestamp = (Date) object[1];
+        if (Objects.isNull(lockUserId)) {
+            Lock lock = new Lock();
+            lock.setRead(false);
+            return lock;
+        }
+        // message which is shown to the user
+        String lockMsg = Constants.LOCK_TEXT.replaceAll("@@USER@@",
+            lockUserId);
+        //if userID is present, the Lock time stamp will also be present
+        //4800000 milliseconds equals to 1.5 hours.
+        // 3600000은 1시간
         Date time = new Date();
-        Lock lck = new Lock();
-        if (list.size() > 0 && list.get(0) != null) {
-            Object[] object = (Object[]) list.get(0);
-            String userId = (String) object[0];
-            Date lockTimestamp = (Date) object[1];
-            if (userId != null) {
-                // message which is shown to the user 
-                String lockMsg = Constants.LOCK_TEXT.replaceAll("@@USER@@",
-                    userId);
-                //if userID is present, the Lock time stamp will also be present
-                //4800000 milliseconds equals to 1 1/2 hours.
-                if (time.getTime() - lockTimestamp.getTime() > 3600000) {
-                    //New user gets lock only on first screen 
-                    //If 1 1/2 hours expires when user is not on 1st screen then for same user lock can be refreshed.
-                    if (firstScreen
-                        || userId.equalsIgnoreCase(user.getUserId())) {
-                        //to set the  access to write mode
-                        lck.setRead(false);
-                        return lck;
-                    }
-                    lck.setRead(true);
-                    //Only read access is permitted to other user
-                    lck.setLockReason(lockMsg);
-                    return lck;
-                } else if (userId.equalsIgnoreCase(user.getUserId())) {
-                    // Locked By Same User, Write access
-                    lck.setRead(false);
-                    return lck;
-                } else {
-                    lck.setRead(true);
-                    //Only Read Access is Permitted
-                    lck.setLockReason(lockMsg);
-                    return lck;
-                }
+        if (time.getTime() - lockTimestamp.getTime() > 3600000) {
+            //New user gets lock only on first screen
+            //If 1 1/2 hours expires when user is not on 1st screen then for same user lock can be refreshed.
+            if (isFirstScreen || lockUserId.equalsIgnoreCase(user.getUserId())) {
+                //to set the  access to write mode
+                final Lock lock = new Lock();
+                lock.setRead(false);
+                return lock;
             }
         }
-        lck.setRead(false);
-        return lck;
+
+        if (lockUserId.equalsIgnoreCase(user.getUserId())) {
+            final Lock lock = new Lock();
+            // Locked By Same User, Write access
+            lock.setRead(false);
+            return lock;
+        }
+
+        Lock lock = new Lock();
+        // firstScreen이 아니며, 문자열이 서로 틀린경우
+        // 유저의 아이디와 입력받는 id가 다른경우 이부분을 수행함
+        lock.setRead(true);
+        //Only Read Access is Permitted
+        lock.setLockReason(lockMsg);
+        return lock;
     }
 
 }
