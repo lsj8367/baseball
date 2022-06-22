88a516295bb5af72eb652b9e1fe1526e4fdec5b9
lsj8367 <lsj8367@naver.com>
2022-06-22 22:54:26 +0900

    :recycle: public, 안쓰는 매개변수 제거

--- clean-code/src/test/java/io/github/lsj8367/refactoring/lock/UserLoginCheckerTest.java
+++ clean-code/src/test/java/io/github/lsj8367/refactoring/lock/UserLoginCheckerTest.java
@@ -4,67 +4,56 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import io.github.lsj8367.refactoring.lock.Lock;
-import io.github.lsj8367.refactoring.lock.User;
-import io.github.lsj8367.refactoring.lock.UserLoginChecker;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class UserLoginCheckerTest {
+class UserLoginCheckerTest {
 
-    UserLoginChecker userLoginChecker = new UserLoginChecker();
+    private final UserLoginChecker userLoginChecker = new UserLoginChecker();
 
     @Test
     @DisplayName("잠겨있는 사용자와 input UserId가 다른경우")
-    public void testisUserAllowedToLogin_DifferentUserTriesImmediatelyAfter() {
-        Object[] access = new Object[]{"TEST_USER_ID_1", LocalDateTime.now()};
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-            "TEST_USER_ID_2"), Arrays.asList(new Object[][]{access}));
+    void testisUserAllowedToLogin_DifferentUserTriesImmediatelyAfter() {
+        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+            "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", LocalDateTime.now()));
         assertTrue(lock.isReadAccess());
         assertNotNull(lock.getLockReason());
     }
 
     @Test
-    public void testisUserAllowedToLogin_SameUserReturnsToFirstScreen() {
-        Object[] access = new Object[]{"TEST_USER_ID", LocalDateTime.now()};
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-            "TEST_USER_ID"), Arrays.asList(new Object[][]{access}));
+    void testisUserAllowedToLogin_SameUserReturnsToFirstScreen() {
+        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+            "TEST_USER_ID"), new LockUser("TEST_USER_ID", LocalDateTime.now()));
         assertFalse(lock.isReadAccess());
         assertNull(lock.getLockReason());
     }
 
     @Test
-    public void testisUserAllowedToLogin_SameUserReturnsToSecondScreen() {
-        Object[] access = new Object[]{"TEST_USER_ID", LocalDateTime.now()};
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", false, new User(
-            "TEST_USER_ID"), Arrays.asList(new Object[][]{access}));
+    void testisUserAllowedToLogin_SameUserReturnsToSecondScreen() {
+        Lock lock = userLoginChecker.isUserAllowedToLogin(false, new User(
+            "TEST_USER_ID"), new LockUser("TEST_USER_ID", LocalDateTime.now()));
         assertFalse(lock.isReadAccess());
         assertNull(lock.getLockReason());
     }
 
     @Test
-    public void testisUserAllowedToLogin_User2TriesToLoginToFirstScreen3hoursAfterUser1() {
-        Object[] access = new Object[]{"TEST_USER_ID_1", threeHoursBefore()};
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-            "TEST_USER_ID_2"), Arrays.asList(new Object[][]{access}));
+    void testisUserAllowedToLogin_User2TriesToLoginToFirstScreen3hoursAfterUser1() {
+        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+            "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", threeHoursBefore()));
         assertFalse(lock.isReadAccess());
         assertNull(lock.getLockReason());
     }
 
     @Test
-    public void testisUserAllowedToLogin_User2TriesToLoginToSecondScreen3hoursAfterUser1() {
-        Object[] access = new Object[]{"TEST_USER_ID_1", threeHoursBefore()};
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", false, new User(
-            "TEST_USER_ID_2"), Arrays.asList(new Object[][]{access}));
+    void testisUserAllowedToLogin_User2TriesToLoginToSecondScreen3hoursAfterUser1() {
+        Lock lock = userLoginChecker.isUserAllowedToLogin(false, new User(
+            "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", threeHoursBefore()));
         assertTrue(lock.isReadAccess());
         assertNotNull(lock.getLockReason());
     }
 
-    public LocalDateTime threeHoursBefore() {
+    private LocalDateTime threeHoursBefore() {
         return LocalDateTime.now().minusHours(3L);
     }
 
