438564e7d733332928f95c02147d8dabcf43f58c
lsj8367 <lsj8367@naver.com>
2022-06-22 23:07:01 +0900

    :recycle: 객체끼리 비교하게끔 테스트 변경

--- clean-code/src/test/java/io/github/lsj8367/refactoring/lock/UserLoginCheckerTest.java
+++ clean-code/src/test/java/io/github/lsj8367/refactoring/lock/UserLoginCheckerTest.java
@@ -1,9 +1,6 @@
 package io.github.lsj8367.refactoring.lock;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,42 +12,46 @@ class UserLoginCheckerTest {
     @Test
     @DisplayName("잠겨있는 사용자와 input UserId가 다른경우")
     void testisUserAllowedToLogin_DifferentUserTriesImmediatelyAfter() {
-        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+        final Lock actual = userLoginChecker.isUserAllowedToLogin(true, new User(
             "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", LocalDateTime.now()));
-        assertTrue(lock.isReadAccess());
-        assertNotNull(lock.getLockReason());
+        final Lock expected = Lock.readLockWithMessage("Locked by TEST_USER_ID_1", true);
+        assertLock(actual, expected);
     }
 
     @Test
     void testisUserAllowedToLogin_SameUserReturnsToFirstScreen() {
-        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+        Lock actual = userLoginChecker.isUserAllowedToLogin(true, new User(
             "TEST_USER_ID"), new LockUser("TEST_USER_ID", LocalDateTime.now()));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        final Lock expected = Lock.writeLock(false);
+        assertLock(actual, expected);
     }
 
     @Test
     void testisUserAllowedToLogin_SameUserReturnsToSecondScreen() {
-        Lock lock = userLoginChecker.isUserAllowedToLogin(false, new User(
+        final Lock actual = userLoginChecker.isUserAllowedToLogin(false, new User(
             "TEST_USER_ID"), new LockUser("TEST_USER_ID", LocalDateTime.now()));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        final Lock expected = Lock.writeLock(false);
+        assertLock(actual, expected);
     }
 
     @Test
     void testisUserAllowedToLogin_User2TriesToLoginToFirstScreen3hoursAfterUser1() {
-        Lock lock = userLoginChecker.isUserAllowedToLogin(true, new User(
+        final Lock actual = userLoginChecker.isUserAllowedToLogin(true, new User(
             "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", threeHoursBefore()));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        final Lock expected = Lock.writeLock(false);
+        assertLock(actual, expected);
     }
 
     @Test
     void testisUserAllowedToLogin_User2TriesToLoginToSecondScreen3hoursAfterUser1() {
-        Lock lock = userLoginChecker.isUserAllowedToLogin(false, new User(
+        final Lock actual = userLoginChecker.isUserAllowedToLogin(false, new User(
             "TEST_USER_ID_2"), new LockUser("TEST_USER_ID_1", threeHoursBefore()));
-        assertTrue(lock.isReadAccess());
-        assertNotNull(lock.getLockReason());
+        final Lock expected = Lock.readLockWithMessage("Locked by TEST_USER_ID_1", true);
+        assertLock(actual, expected);
+    }
+
+    private void assertLock(final Lock actual, final Lock expected) {
+        assertThat(actual).isEqualTo(expected);
     }
 
     private LocalDateTime threeHoursBefore() {
