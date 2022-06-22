f32193aa4cc05f120d40d8136d7bc3e788d931a0
lsj8367 <lsj8367@naver.com>
2022-06-22 17:56:11 +0900

    :recycle: 조건 메소드로 분리

--- clean-code/src/main/java/io.github.lsj8367/c/refactoring/lock/UserLoginChecker.java
+++ clean-code/src/main/java/io.github.lsj8367/c/refactoring/lock/UserLoginChecker.java
@@ -6,56 +6,53 @@ import java.util.Objects;
 
 public class UserLoginChecker {
 
-    /**
-     * {@inheritDoc}.
-     */
+    public static final int LOCK_FOR_THREE_HOURS = 3600000;
+
     public Lock isUserAllowedToLogin(long id, String status,
-        boolean isFirstScreen, User user, List<Object> list) {
-        if (list.isEmpty()) {
-            Lock lock = new Lock();
-            lock.setRead(false);
-            return lock;
+        boolean isFirstScreen, User loginUserRequest, List<Object> existingUsers) {
+        if (existingUsers.isEmpty()) {
+            return writeLock();
         }
 
-        Object[] object = (Object[]) list.get(0);
-        String lockUserId = (String) object[0];
-        Date lockTimestamp = (Date) object[1];
+        Object[] existingUser = (Object[]) existingUsers.get(0);
+        String lockUserId = (String) existingUser[0];
+        Date lockTimestamp = (Date) existingUser[1];
+
         if (Objects.isNull(lockUserId)) {
-            Lock lock = new Lock();
-            lock.setRead(false);
-            return lock;
+            return writeLock();
         }
-        // message which is shown to the user
-        String lockMsg = Constants.LOCK_TEXT.replaceAll("@@USER@@",
-            lockUserId);
+
+        if (lockUserId.equalsIgnoreCase(loginUserRequest.getUserId())) {
+            return writeLock();
+        }
+
         //if userID is present, the Lock time stamp will also be present
         //4800000 milliseconds equals to 1.5 hours.
         // 3600000은 1시간
-        Date time = new Date();
-        if (time.getTime() - lockTimestamp.getTime() > 3600000) {
-            //New user gets lock only on first screen
-            //If 1 1/2 hours expires when user is not on 1st screen then for same user lock can be refreshed.
-            if (isFirstScreen || lockUserId.equalsIgnoreCase(user.getUserId())) {
-                //to set the  access to write mode
-                final Lock lock = new Lock();
-                lock.setRead(false);
-                return lock;
-            }
+        if (isFirstScreen && new Date().getTime() - lockTimestamp.getTime() > LOCK_FOR_THREE_HOURS) {
+            return writeLock();
         }
 
-        if (lockUserId.equalsIgnoreCase(user.getUserId())) {
-            final Lock lock = new Lock();
-            // Locked By Same User, Write access
-            lock.setRead(false);
-            return lock;
-        }
+        return readLockWithMessage(lockUserId);
+    }
 
-        Lock lock = new Lock();
+    private Lock readLockWithMessage(final String lockUserId) {
         // firstScreen이 아니며, 문자열이 서로 틀린경우
         // 유저의 아이디와 입력받는 id가 다른경우 이부분을 수행함
+
+        // 사용자한테 표시되는 메시지
+        final String lockMsg = Constants.LOCK_TEXT.replaceAll("@@USER@@", lockUserId);
+        final Lock lock = new Lock();
         lock.setRead(true);
-        //Only Read Access is Permitted
+        // 읽기 잠금
         lock.setLockReason(lockMsg);
+        return lock;
+    }
+
+    private Lock writeLock() {
+        // Locked By Same User, Write access
+        final Lock lock = new Lock();
+        lock.setRead(false);
         return lock;
     }
 
