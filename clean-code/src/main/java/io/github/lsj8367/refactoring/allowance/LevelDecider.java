9409754372108910a228fee41c9cbde6d38ec750
lsj8367 <lsj8367@naver.com>
2022-06-23 09:56:38 +0900

    :recycle: 수당 할당 로직 분리

--- clean-code/src/main/java/io/github/lsj8367/refactoring/allowance/LevelDecider.java
+++ clean-code/src/main/java/io/github/lsj8367/refactoring/allowance/LevelDecider.java
@@ -2,6 +2,8 @@ package io.github.lsj8367.refactoring.allowance;
 
 import io.github.lsj8367.refactoring.Log;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
 
 
 public class LevelDecider {
@@ -11,32 +13,45 @@ public class LevelDecider {
         Allowance allowance, BigDecimal allowanceVal) {
         String Level = "";
         boolean isAllowanceDiffPercLessConfig = false;
+
+
         if (fileVersion.getPrevFileVersionStatus().equals(
             Constants.FILE_STATUS_CLOSED)) {
-            Level = Constants.LEVEL_D2;
-        } else if (allowanceVal != null && fileVersionIdPrev != 0
+            return Constants.LEVEL_D2;
+        }
+
+        if (allowanceVal != null && fileVersionIdPrev != 0
             && allowance.getAllowanceId() != 0) {
             BigDecimal allowancePrevVal = allowance.getAllowanceValue();
             Log.debug("Previous Allowance value = # {0}", allowancePrevVal);
+
+
+
             // calculate Allowance Diff
             BigDecimal allowanceDiff = allowanceVal.subtract(allowancePrevVal);
             Log.debug("Allowance Difference value = # {0}", allowanceDiff);
             Log.logApplicationDebug(" Level :" + Level,
                 getClass());
-            BigDecimal allowanceDiffPerc = Constants.BIG_DECIMAL_ZERO;
+
+
+
             // Get configured Allowance limit value & allowance difference percentage
-            BigDecimal configuredDiffPerc = new BigDecimal(
+            BigDecimal configuredDiffPerc = BigDecimal.valueOf(
                 Constants.CONFIGURATION_ALLOWANCE_PERCENT);
-            BigDecimal configuredDiffAmt = new BigDecimal(
+            BigDecimal configuredDiffAmt = BigDecimal.valueOf(
                 Constants.CONFIGURATION_ALLOWANCE_LIMIT);
             // calculate Allowance Diff Percentage
+
+            final BigDecimal allowanceDiffPerc = calculateAllowanceDiffPercent(allowancePrevVal, allowanceDiff);
+
+
+
             if (allowancePrevVal != null && allowancePrevVal.doubleValue() != 0) {
-                allowanceDiffPerc = allowanceDiff.abs()
-                    .divide(allowancePrevVal, 2, BigDecimal.ROUND_HALF_UP)
-                    .multiply(Constants.BIG_DECIMAL_HUNDRED);
                 isAllowanceDiffPercLessConfig = allowanceDiffPerc
                     .compareTo(configuredDiffPerc) < 1;
             }
+
+
 
             Log.logApplicationInfo(
                 "Allowance Diff : " + allowanceDiff.doubleValue(),
@@ -44,19 +59,31 @@ public class LevelDecider {
             Log.logApplicationInfo("Allowance Diff Perc : "
                     + allowanceDiffPerc.doubleValue(), allowanceDiffPerc,
                 getClass());
+
+
+
             if (allowanceDiff.abs().compareTo(configuredDiffAmt) < 1
                 || isAllowanceDiffPercLessConfig) {
-                Level = Constants.LEVEL_D1;
-            } else {
-                Level = Constants.LEVEL_D2;
+                return Constants.LEVEL_D1;
             }
-        } else {
-            // if prev file  or allowance for previous file 
-            // does not exsits
-            // then  level is D2
-            Level = Constants.LEVEL_D2;
+
+            return Constants.LEVEL_D2;
         }
-        return Level;
+
+        // if prev file  or allowance for previous file
+        // does not exsits
+        // then  level is D2
+        return Constants.LEVEL_D2;
+    }
+
+    private BigDecimal calculateAllowanceDiffPercent(final BigDecimal allowancePrevVal, final BigDecimal allowanceDiff) {
+        if (Objects.isNull(allowancePrevVal) || allowancePrevVal.doubleValue() == 0) {
+            return Constants.BIG_DECIMAL_ZERO;
+        }
+
+        return allowanceDiff.abs()
+            .divide(allowancePrevVal, 2, RoundingMode.HALF_UP)
+            .multiply(Constants.BIG_DECIMAL_HUNDRED);
     }
 
 }
