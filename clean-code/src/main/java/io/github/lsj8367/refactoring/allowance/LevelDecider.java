06c29d2562f4abb3bba64226647cd5f298f92bf1
lsj8367 <lsj8367@naver.com>
2022-06-25 13:51:09 +0900

    :recycle: 상수, enum로직 변경

--- clean-code/src/main/java/io/github/lsj8367/refactoring/allowance/LevelDecider.java
+++ clean-code/src/main/java/io/github/lsj8367/refactoring/allowance/LevelDecider.java
@@ -8,47 +8,31 @@ import java.util.Objects;
 
 public class LevelDecider {
 
-    public String determineLevel(File fileVersion,
-        long fileVersionIdPrev,
-        Allowance allowance, BigDecimal allowanceVal) {
-        String Level = "";
+    public String determineLevel(final File fileVersion,
+        final long fileVersionIdPrev, final Allowance allowance, final BigDecimal inputAllowance) {
         boolean isAllowanceDiffPercLessConfig = false;
 
-
-        if (fileVersion.getPrevFileVersionStatus().equals(
-            Constants.FILE_STATUS_CLOSED)) {
+        if (fileVersion.isFileClosed()) {
             return Constants.LEVEL_D2;
         }
 
-        if (allowanceVal != null && fileVersionIdPrev != 0
+        if (inputAllowance != null && fileVersionIdPrev != 0
             && allowance.getAllowanceId() != 0) {
             BigDecimal allowancePrevVal = allowance.getAllowanceValue();
             Log.debug("Previous Allowance value = # {0}", allowancePrevVal);
 
-
-
             // calculate Allowance Diff
-            BigDecimal allowanceDiff = allowanceVal.subtract(allowancePrevVal);
+            BigDecimal allowanceDiff = inputAllowance.subtract(allowancePrevVal);
             Log.debug("Allowance Difference value = # {0}", allowanceDiff);
-            Log.logApplicationDebug(" Level :" + Level,
-                getClass());
 
-
-
-            // Get configured Allowance limit value & allowance difference percentage
-            BigDecimal configuredDiffPerc = BigDecimal.valueOf(
-                Constants.CONFIGURATION_ALLOWANCE_PERCENT);
-            BigDecimal configuredDiffAmt = BigDecimal.valueOf(
-                Constants.CONFIGURATION_ALLOWANCE_LIMIT);
             // calculate Allowance Diff Percentage
 
             final BigDecimal allowanceDiffPerc = calculateAllowanceDiffPercent(allowancePrevVal, allowanceDiff);
 
-
-
+            // Get configured Allowance limit value & allowance difference percentage
             if (allowancePrevVal != null && allowancePrevVal.doubleValue() != 0) {
                 isAllowanceDiffPercLessConfig = allowanceDiffPerc
-                    .compareTo(configuredDiffPerc) < 1;
+                    .compareTo(AllowanceConfig.PERCENT.getValue()) < 1;
             }
 
 
@@ -60,9 +44,7 @@ public class LevelDecider {
                     + allowanceDiffPerc.doubleValue(), allowanceDiffPerc,
                 getClass());
 
-
-
-            if (allowanceDiff.abs().compareTo(configuredDiffAmt) < 1
+            if (allowanceDiff.abs().compareTo(AllowanceConfig.LIMIT.getValue()) < 1
                 || isAllowanceDiffPercLessConfig) {
                 return Constants.LEVEL_D1;
             }
