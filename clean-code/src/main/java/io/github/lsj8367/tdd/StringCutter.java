6706225cf321df4581c6f069b358400269956468
lsj8367 <lsj8367@naver.com>
2022-06-28 15:36:38 +0900

    :recycle: record 클래스 타입으로 변경

--- clean-code/src/main/java/io/github/lsj8367/tdd/StringCutter.java
+++ clean-code/src/main/java/io/github/lsj8367/tdd/StringCutter.java
@@ -1,11 +1,11 @@
 package io.github.lsj8367.tdd;
 
-public class StringCutter {
+public record StringCutter(String str) {
 
     private static final String CUT_TARGET = "A";
     private static final String BLANK = "";
 
-    public String removeAFirstTwoCharacter(final String str) {
+    public String removeAFirstTwoCharacter() {
 
         if (str.length() < 2) {
             return str.replace("A", "");
--- clean-code/src/test/java/io/github/lsj8367/tdd/StringCutterTest.java
+++ clean-code/src/test/java/io/github/lsj8367/tdd/StringCutterTest.java
@@ -3,7 +3,6 @@ package io.github.lsj8367.tdd;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,13 +10,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class StringCutterTest {
-
-    private StringCutter stringCutter;
-
-    @BeforeEach
-    void setUp() {
-        stringCutter = new StringCutter();
-    }
 
     @ParameterizedTest
     @MethodSource
@@ -27,25 +19,28 @@ class StringCutterTest {
     }
 
     private static Stream<Arguments> ifFirstTwoCharactersAreAThenCut() {
-        final StringCutter stringCutter = new StringCutter();
         return Stream.of(
-            Arguments.of(stringCutter.removeAFirstTwoCharacter("AACD"), "CD"),
-            Arguments.of(stringCutter.removeAFirstTwoCharacter("ABCD"), "BCD"),
-            Arguments.of(stringCutter.removeAFirstTwoCharacter("BACD"), "BCD"),
-            Arguments.of(stringCutter.removeAFirstTwoCharacter("MNAA"), "MNAA"),
-            Arguments.of(stringCutter.removeAFirstTwoCharacter("AAAA"), "AA")
+            Arguments.of(new StringCutter("AACD").removeAFirstTwoCharacter(), "CD"),
+            Arguments.of(new StringCutter("ABCD").removeAFirstTwoCharacter(), "BCD"),
+            Arguments.of(new StringCutter("BACD").removeAFirstTwoCharacter(), "BCD"),
+            Arguments.of(new StringCutter("MNAA").removeAFirstTwoCharacter(), "MNAA"),
+            Arguments.of(new StringCutter("AAAA").removeAFirstTwoCharacter(), "AA")
         );
     }
 
     @Test
     @DisplayName("앞 두글자중 A가 없으면 그대로 유지")
     void ifNotFirstTwoCharactersAreAThenKeep() {
-        assertThat(stringCutter.removeAFirstTwoCharacter("BDAA")).isEqualTo("BDAA");
+        final StringCutter stringCutter = new StringCutter("BDAA");
+
+        assertThat(stringCutter.removeAFirstTwoCharacter()).isEqualTo("BDAA");
     }
 
     @Test
     @DisplayName("문자열의 총길이가 2보다 작은경우면 A를 전부 빈 문자열로 변환")
     void stringLengthLessThan2ThenConvertEmptyStr() {
-        assertThat(stringCutter.removeAFirstTwoCharacter("A")).isEmpty();
+        final StringCutter stringCutter = new StringCutter("A");
+
+        assertThat(stringCutter.removeAFirstTwoCharacter()).isEmpty();
     }
 }
