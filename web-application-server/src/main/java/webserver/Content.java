6b752679693d366ae68c9fb0bac28ff7a53de130
lsj8367 <lsj8367@naver.com>
2023-01-14 17:28:41 +0900

    :sparkles: post방식으로 변경

---
+++ web-application-server/src/main/java/webserver/Content.java
@@ -0,0 +1,18 @@
+package webserver;
+
+public final class Content {
+    private int length;
+
+    public Content(final int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(final int length) {
+        this.length = length;
+    }
+
+}
